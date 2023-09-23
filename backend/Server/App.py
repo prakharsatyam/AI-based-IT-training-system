31bd7a1de1a382ed9a112a2eacff6c2f45404a15
nabinghosh <www.champaghosha.12345@gmail.com>
2023-09-23 07:10:24 +0530

    fetching data from backend server to home page

--- backend/Server/App.py
+++ backend/Server/App.py
@@ -1,29 +1,83 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-# import joblib
-# import numpy as np
-# from PIL import Image
-# from io import BytesIO
+import numpy as np
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+
+
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/database_name'
+mongo = PyMongo(app)
+
+# @app.route('/get_topics_and_subtopics', methods=['GET'])
+# def get_topics_and_subtopics():
+#     collection = mongo.db.collection_name
+    
+#     cursor = collection.find({}, {'_id': 0, 'topic 1': ['Subtopic 1.1', 'Subtopic 1.2', 'Subtopic 1.3', 'Subtopic 1.4']})
+    
+#     syllabus = {
+#     'Topic 1': ['Subtopic 1.1', 'Subtopic 1.2', 'Subtopic 1.3', 'Subtopic 1.4'],
+#     'Topic 2': ['Subtopic 2.1', 'Subtopic 2.2', 'Subtopic 2.3'],
+#     'Topic 3': ['Subtopic 3.1', 'Subtopic 3.2', 'Subtopic 3.3', 'Subtopic 3.4', 'Subtopic 3.5'],
+# }
+
+#     response = {
+#         'topics': topics,
+#         'subtopics': subtopics
+#     }
+    
+#     return jsonify(response)
+
+@app.route('/recommend_module/<user_id>/<state>', methods=['GET'])
+def recommend_module_endpoint(user_id, state):
+    recommended_module = recommend_module(user_id, state)
+    return jsonify({'recommended_module': recommended_module})
+
+syllabus = {
+    'Topic 1': ['Subtopic 1.1', 'Subtopic 1.2', 'Subtopic 1.3', 'Subtopic 1.4'],
+    'Topic 2': ['Subtopic 2.1', 'Subtopic 2.2', 'Subtopic 2.3'],
+    'Topic 3': ['Subtopic 3.1', 'Subtopic 3.2', 'Subtopic 3.3', 'Subtopic 3.4', 'Subtopic 3.5'],
+}
+
+states = list(syllabus.keys())  
+num_actions = len(syllabus[states[0]])  
+
+q_table = np.zeros((len(states), num_actions))
+
+learning_rate = 0.1
+discount_factor = 0.9
+exploration_prob = 0.3
+
+def update_q_table(state, action, reward, next_state):
+    best_next_action = np.argmax(q_table[next_state])
+    q_table[state][action] = (1 - learning_rate) * q_table[state][action] + \
+                            learning_rate * (reward + discount_factor * q_table[next_state][best_next_action])
+
+def determine_next_subtopics(current_topic, q_table):
+    state = states.index(current_topic)
+    action = np.argmax(q_table[state])
+    next_subtopic = syllabus[current_topic][action]
+    
+    return current_topic, next_subtopic
+
+current_topic = 'Topic 1'  
+num_episodes = 1000  
+
+@app.route('/get_next_subtopic/<current_topic>', methods=['GET'])
+def get_next_subtopic(current_topic):
+    if current_topic not in states:
+        return jsonify({"message": "Invalid topic"}), 400
+
+    current_topic, next_subtopic = determine_next_subtopics(current_topic, q_table)
+
+    if next_subtopic:
+        return jsonify({"next_subtopic": next_subtopic}), 200
+    else:
+        return jsonify({"message": "Syllabus is complete"}), 200
 
 app = Flask(__name__)
 CORS(app)
-
-
-@app.route('/predict', methods=['GET'])
-def predict():
-    return jsonify({
-        'predictions': [
-            {
-                'label': 'Man',
-                'probability': 0.83333
-            },
-            {
-                'label': 'Woman',
-                'probability': 0.16667
-            }
-        ]
-    })
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=9009) #Running on http://127.0.0.1:9009
--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,101 +1,37 @@
-import React,{useEffect, useState} from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default function Home() {
-    const [predictions, setPredictions] = useState([]);
-    const [loading, setLoading] = useState(false);
+function Home() {
+  const [currentTopic, setCurrentTopic] = useState('Topic 1'); 
+  const [nextSubtopic, setNextSubtopic] = useState('');
 
-    useEffect(() => {
-        
-        const fetchPredictions = async () => {
-            try {
-                setLoading(true);
-                const response = await axios.get('http://localhost:9009/predict');
-                setPredictions(response.data.predictions);
-            } catch (error) {
-                console.error('Error fetching predictions:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const apiUrl = `http://localhost:9009/get_next_subtopic/${currentTopic}`;
 
-        
-        fetchPredictions();
-    }, []);
+    axios.get(apiUrl)
+      .then((response) => {
+        if (response.status === 200) {
+          setNextSubtopic(response.data.next_subtopic);
+        } else {
+          console.error('Failed to fetch data');
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, [currentTopic]);
 
-    return (
+  return (
+    <div>
+      <h1>Current Topic: {currentTopic}</h1>
+      {nextSubtopic && (
         <div>
-            <h1>Image Classification</h1>
-            {loading ? (
-                <p>Loading predictions...</p>
-            ) : (
-                <div>
-                    <h2>Predictions:</h2>
-                    <ul>
-                        {predictions.map((prediction, index) => (
-                            <li key={index}>
-                                <strong>Label:</strong> {prediction.label}
-                                <br />
-                                <strong>Probability:</strong> {prediction.probability}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+          <h2>Next Subtopic:</h2>
+          <p>{nextSubtopic}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
-
-
-// -----------------------------------------------------------------------------------
-
-// import { useState } from 'react';
-// import axios from 'axios';
-
-// export default function Home() {
-//     const [file, setFile] = useState(null);
-//     const [predictions, setPredictions] = useState([]);
-
-//     const handleFileUpload = (event) => {
-//         const selectedFile = event.target.files[0];
-//         setFile(selectedFile);
-//     };
-
-//     const handlePredict = async () => {
-//         if (!file) {
-//             alert('Please select an image first.');
-//             return;
-//         }
-
-//         const formData = new FormData();
-//         formData.append('image', file);
-
-//         try {
-//             const response = await axios.post('/predict', formData, {
-//                 headers: {
-//                     'Content-Type': 'multipart/form-data',
-//                 },
-//             });
-//             setPredictions(response.data.predictions);
-//         } catch (error) {
-//             console.error('Error making prediction request:', error);
-//         }
-//     };
-
-//     return (
-//         <div>
-//             <h1>Image Classification</h1>
-//             <input type="file" accept="image/*" onChange={handleFileUpload} />
-//             <button onClick={handlePredict}>Predict</button>
-//             <div>
-//                 <h2>Predictions:</h2>
-//                 <ul>
-//                     {predictions.map((pred, index) => (
-//                         <li key={index}>{pred}</li>
-//                     ))}
-//                 </ul>
-//             </div>
-//         </div>
-//     );
-// }
+export default Home;
